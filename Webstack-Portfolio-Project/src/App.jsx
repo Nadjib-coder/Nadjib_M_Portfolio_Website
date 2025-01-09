0ccf2bc0d33a9add48a9e58dad14157a18cbedf6
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-01-09 15:44:36 +0000

    add 3 other frontend projects

--- Webstack-Portfolio-Project/src/App.jsx
+++ Webstack-Portfolio-Project/src/App.jsx
@@ -4,13 +4,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
-
-{
-  /*
 import Contact from './components/Contact';
-*/
-}
-
 import './App.css';
 
 export default function App() {
@@ -21,9 +15,7 @@ export default function App() {
       <About />
       <Skills />
       <Projects />
-      {/*}
       <Contact />
-      */}
       <Footer />
     </>
   );
